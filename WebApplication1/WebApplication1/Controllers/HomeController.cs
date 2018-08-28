8e47e6b02285eaed7e75a327070c13c958c5e496
CD <CD@CDD.CO>
2018-08-27 20:54:08 -0400

    See description
    Using .net core
    Connect to a local db using entity framework core and fetching data

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private QAPracticeContext context = new QAPracticeContext();
+        private readonly QAPracticeContext _context;
+
+        public HomeController(QAPracticeContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ManageTeamLeaders()
+        {
+            var teamLeaders = _context.TeamLeaders.ToList();
+            return View(teamLeaders);
+
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/QAPracticeContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class QAPracticeContext : DbContext
+    {
+        public QAPracticeContext(DbContextOptions<QAPracticeContext> options)
+            : base(options)
+        { }
+
+        public DbSet<TeamLeader> TeamLeaders { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=QATestDb;Trusted_Connection=True;");
+        //}
+    }
+
+    public class TeamLeader
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? TeamId { get; set; }
+    }
+
+}
